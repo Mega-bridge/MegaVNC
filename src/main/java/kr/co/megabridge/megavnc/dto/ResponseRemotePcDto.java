e674ae61e6574bb16f42d8a417a5375f647e264e
Ted <bflook08307@gmail.com>
2024-03-26 11:22:12 +0900

    add: ResponseRemotePcDto 가 커밋 누락되어서 추가

---
+++ src/main/java/kr/co/megabridge/megavnc/dto/ResponseRemotePcDto.java
@@ -0,0 +1,12 @@
+package kr.co.megabridge.megavnc.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseRemotePcDto {
+
+    private Long repeaterId;
+
+}
