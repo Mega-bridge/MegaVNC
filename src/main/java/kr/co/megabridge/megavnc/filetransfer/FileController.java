2c8ed7fae5485a2771de9fa3ec0781fb1b9f6b2d
iin <dldls9709@naver.com>
2024-06-17 10:45:34 +0900

    파일설치/등록 페이지 추가, 이에 필요한 css, js추가

---
+++ src/main/java/kr/co/megabridge/megavnc/filetransfer/FileController.java
@@ -0,0 +1,33 @@
+package kr.co.megabridge.megavnc.filetransfer;
+
+import kr.co.megabridge.megavnc.domain.Group;
+import kr.co.megabridge.megavnc.security.User;
+import kr.co.megabridge.megavnc.service.RemotePcService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.ui.Model;
+
+import java.util.List;
+
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/files")
+@Slf4j
+public class FileController {
+
+    private final RemotePcService remotePcService;
+
+    @GetMapping
+    public String showFiles(@AuthenticationPrincipal User user, Model model) {
+        List<Group> groups = remotePcService.findGroupByMember(user);
+        log.info("username = {}", user.getUsername());
+        model.addAttribute("user", user);
+        model.addAttribute("groups", groups);
+        return "files";
+    }
+
+}
